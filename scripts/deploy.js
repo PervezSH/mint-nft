20ae1a266a3615471e1bff584fa5a521e6be1bf0
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2021-12-16 20:11:40 +0530

    deploy to rinkeby testnet
    - create deploy.js

---
+++ scripts/deploy.js
@@ -0,0 +1,31 @@
+const main = async () => {
+    // Compile contract and generates necessary files under the artifacts directory
+    const nftContractFactory = await hre.ethers.getContractFactory('MyNFT');
+    const nftContract = await nftContractFactory.deploy();
+    // wait for contract to be deployed
+    await nftContract.deployed();
+
+    console.log("Contract deployed to : ", nftContract.address);
+
+    // Mint NFT
+    let txn = await nftContract.makeAnNFT();
+    await txn.wait();
+    console.log("Minted NFT #1");
+    
+    // Mint another one for fun
+    txn = await nftContract.makeAnNFT();
+    await txn.wait();
+    console.log("Minted NFT #2");
+};
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+runMain();
