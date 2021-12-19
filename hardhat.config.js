77e79b883fdce6f0a34b262bea22c95cc6e29cc9
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2021-12-19 10:58:21 +0530

    verify contract on etherscan

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 require('dotenv').config();
 
 module.exports = {
@@ -8,5 +9,8 @@ module.exports = {
       url: process.env.URL,
       accounts: [process.env.ACCOUNTS],
     },
-  }
+  },
+  etherscan: {
+    apiKey: process.env.APIKEY,
+  },
 };
